ab0fabdf385ff8d185879c36e571174eed5e1116
Tim Guo <timguoqk@users.noreply.github.com>
2015-05-01 11:44:02 -0700

    clean_json

--- gdoc-json-parser.py
+++ gdoc-json-parser.py
@@ -3,6 +3,18 @@ import urllib
 import json
 from optparse import OptionParser
 
+
+def clean_json(raw_data):
+    entries = raw_data['feed']['entry']
+    cols = [x for x in entries[0] if x.startswith('gsx$')]
+    data = []
+    for entry in entries:
+        row = {}
+        for col in cols:
+            row[col[4:]] = entry[col]['$t']
+        data.append(row)
+    return data
+
 if __name__ == '__main__':
     version_msg = "%prog 0.1"
     usage_msg = "%prog [OPTION] URL OUTPUT\n" + \
@@ -19,8 +31,8 @@ if __name__ == '__main__':
         url_id + "/od6/public/values?alt=json"
     response = urllib.urlopen(url)
     # TODO: try and catch
-    data = json.loads(response.read())
-
+    raw_data = json.loads(response.read())
+    data = clean_json(raw_data)
     # TODO: clean data
     output_file = open(args[1], 'w')
     output_file.write(data)
