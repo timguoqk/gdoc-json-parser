530b8feee334961ba3c81c54ad61e4c25379c1b9
Tim Guo <timguoqk@users.noreply.github.com>
2015-05-03 23:48:07 -0700

    remove optparser, add setup fields

--- gdoc-json-parser.py
+++ gdoc-json-parser.py
@@ -1,7 +1,6 @@
 import sys
 import urllib
 import json
-from optparse import OptionParser
 
 
 def clean_json(raw_data):
@@ -16,24 +15,28 @@ def clean_json(raw_data):
     return data
 
 if __name__ == '__main__':
-    version_msg = "%prog 0.1"
-    usage_msg = "%prog [OPTION] URL OUTPUT\n" + \
-        "Convert spreadsheet in URL to json."
-    parser = OptionParser(version=version_msg,
-                          usage=usage_msg)
+    if len(sys.argv) == 0:
+        usage_msg = "%prog URL OUTPUT\n" + \
+            "Convert spreadsheet in URL to json."
+        print(usage_msg)
+        return
+    if len(sys.argv) != 1 and len(sys.argv) != 2:
+        print("wrong number of operands")
 
-    options, args = parser.parse_args(sys.argv[1:])
-    if len(args) != 1 and len(args) != 2:
-        parser.error("wrong number of operands")
-
-    url_id = args[0].split('/')[-1]
+    url_id = sys.argv[0].split('/')[-1]
     url = "https://spreadsheets.google.com/feeds/list/" + \
         url_id + "/od6/public/values?alt=json"
-    response = urllib.urlopen(url)
-    # TODO: try and catch
+    try:
+        response = urllib.urlopen(url)
+    except Exception as e:
+        print("failed to open url")
+        raise e
+
     raw_data = json.loads(response.read())
     data = clean_json(raw_data)
-    # TODO: clean data
-    output_file = open(args[1], 'w')
+    if len(sys.argv) == 1:
+        output_file = open('out.json', 'w')
+    else:
+        output_file = open(sys.argv[1], 'w')
     output_file.write(data)
     output_file.close()
--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from distutils.core import setup
 
-# Follow:
-# https://docs.python.org/3.4/distutils/introduction.html
-
+# TODO: complete options
 setup(name='gdoc-json-parser',
       version='0.1',
+      description='',
+      author='Tim Guo',
+      url='https://github.com/timguoqk/gdoc-json-parser',
+      classifiers='',
+      keywords='',
+      license='MIT',
       py_modules=['gdoc-json-parser'],
       )
-# TODO: more options available
